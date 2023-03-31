0cf85a3f714948beb323a1199e7ea7600ef8ccc4
JordanSgrigna <kaliwyrdiscord@gmail.com>
2023-03-31 12:57:13 +0200

    Form input

--- js/main.js
+++ js/main.js
@@ -1,50 +1,52 @@
-// USER INFO
-let userName = prompt("Scrivi il tuo nome:");
-let userSurname = prompt("Scrivi il tuo cognome:");
-let userAge = prompt("Quanti anni hai?");
-let userKilometers = prompt("Per quanti kilometri devi viaggiare?");
+function submitForm(event){
+    event.preventDefault();
 
-// INPUT VALIDATION
-let isUserAgeANumber = !isNaN(userAge);
-let isUserKilometersANumber = !isNaN(userKilometers);
+    // USER INFO
+    let userName = document.getElementById("userName").value;
+    let userSurname = document.getElementById("userSurname").value;
+    let userAge = document.getElementById("userAge").value;
+    let userKilometers = document.getElementById("userKilometers").value;
 
-// PRICE PER KILOMETER
-userAge = parseInt(userAge);
-userKilometers = parseInt(userKilometers);
+    // INPUT VALIDATION
+    let isUserAgeANumber = !isNaN(userAge);
+    let isUserKilometersANumber = !isNaN(userKilometers);
 
-let pricePerKilometer = 0.21;
-let userTicketPrice = (userKilometers * pricePerKilometer);
+    // PRICE PER KILOMETER
+    userAge = parseInt(userAge);
+    userKilometers = parseInt(userKilometers);
 
+    let pricePerKilometer = 0.21;
+    let userTicketPrice = (userKilometers * pricePerKilometer);
 
+    if (isUserAgeANumber && isUserKilometersANumber) {
 
-if (isUserAgeANumber && isUserKilometersANumber) {
-
-    if (userAge > 65) {
-        // 40% Discount if age > 65
-        let userTicketPrice40Discount = (userTicketPrice * 60 / 100);
-        console.log("Il prezzo standard è di : " + userTicketPrice);
-        console.log("Applicando lo sconto del 40% per gli over 65: " + userTicketPrice40Discount);
-    
-        document.getElementById("discountedprice").innerHTML = "Scontato per gli over 65: " + userTicketPrice40Discount.toFixed(2) + "&euro;";
-    } else if (userAge < 18) {
-        // 20% Discount if age < 18
-        let userTicketPrice20Discount = (userTicketPrice * 80 / 100);
-        console.log("Il prezzo standard è di : " + userTicketPrice);
-        console.log("Applicando lo sconto del 20% per i minorenni: " + userTicketPrice20Discount);
+        if (userAge > 65) {
+            // 40% Discount if age > 65
+            let userTicketPrice40Discount = (userTicketPrice * 60 / 100);
+            console.log("Il prezzo standard è di : " + userTicketPrice);
+            console.log("Applicando lo sconto del 40% per gli over 65: " + userTicketPrice40Discount);
         
-        document.getElementById("discountedprice").innerHTML = "Scontato per minorenni: " + userTicketPrice20Discount.toFixed(2) + "&euro;";
+            document.getElementById("discountedprice").innerHTML = "Scontato per gli over 65: " + userTicketPrice40Discount.toFixed(2) + "&euro;";
+        } else if (userAge < 18) {
+            // 20% Discount if age < 18
+            let userTicketPrice20Discount = (userTicketPrice * 80 / 100);
+            console.log("Il prezzo standard è di : " + userTicketPrice);
+            console.log("Applicando lo sconto del 20% per i minorenni: " + userTicketPrice20Discount);
+            
+            document.getElementById("discountedprice").innerHTML = "Scontato per minorenni: " + userTicketPrice20Discount.toFixed(2) + "&euro;";
+        } else {
+            console.log("Non hai sconti attivi: " + userTicketPrice);
+            document.getElementById("discountedprice").innerHTML = "Non hai sconti attivi";
+        }
+            
+        
     } else {
-        console.log("Non hai sconti attivi: " + userTicketPrice);
-        document.getElementById("discountedprice").innerHTML = "Non hai sconti attivi";
+        console.log("Inserisci informazioni con i numeri");
     }
-        
-    
-} else {
-    console.log("Inserisci informazioni con i numeri");
+
+    document.getElementById("name").innerHTML = userName;
+    document.getElementById("surname").innerHTML = userSurname;
+    document.getElementById("age").innerHTML = userAge;
+    document.getElementById("normalprice").innerHTML = "Prezzo: " + userTicketPrice.toFixed(2) + "&euro;";
 }
 
-
-document.getElementById("name").innerHTML = userName;
-document.getElementById("surname").innerHTML = userSurname;
-document.getElementById("age").innerHTML = userAge;
-document.getElementById("normalprice").innerHTML = "Prezzo: " + userTicketPrice.toFixed(2) + "&euro;";
